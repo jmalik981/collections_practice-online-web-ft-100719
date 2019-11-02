078d8d466a8ee04b4e08271c46b68dbb225c6ddd
Junaid Malik <jmalik981@gmail.com>
2019-11-02 22:08:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,11 +33,6 @@ def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
-
-  # using select method
-    # array.select do |word|
-    #   word[0] == "a"
-    # end
 end
 
 def sum_array(array)
@@ -46,17 +41,6 @@ def sum_array(array)
     sum+=num
   end
   sum
-
-  # using reduce method
-    # array.reduce(:+)
-
-  # using inject method (short)
-     # array.inject(:+)
-
-  # using inject method (long)
-     # array.inject do |sum,x|
-     #  sum + x
-     # end
 end
 
 def add_s(array)
